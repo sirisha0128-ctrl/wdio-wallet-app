ec6fec3cbf4acb6df3affb4738c2508c6281627b
sirisha <sirisha0128@gmail.com>
2026-02-07 13:01:27 -0800

    Add missing config files and update lock files

---
+++ config/environments.ts/environments.ts
@@ -0,0 +1,24 @@
+
+
+
+
+export interface EnvironmentConfig {
+    baceUrl: string:
+ }
+ 
+ export function getenvironmentconfig(
+    envName: string = 'local'
+  ): EnvironmentConfig {
+  switch (envName.toLowercase()) {
+   case 'local':
+    return { baseUrl: 'http://localhost:3000'};
+   case 'dev':
+    return { baseUrl: 'http://dev.wallet.treetracker.org'};
+   case 'prod':
+    return { baseUrl: 'http://wallet.treetracker.org'};
+   default:
+    throw new Error(
+      'Unknown environment: ${envName}. Available environments: local, dev, prod'
+     );
+   }
+  }
